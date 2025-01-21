60c409d06140ae24711354f696d9b2d505c8f4d3
Jatinsinghv64 <101871839+Jatinsinghv64@users.noreply.github.com>
2025-01-21 20:27:08 +0530

    Create OddEven.c

---
+++ OddEven.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main () 
+{
+    int a;
+    printf("Enter The Number ");
+    scanf("%d",&a);
+    if (a%2==0){
+        printf("Number is Even ");
+    }
+    else {
+        printf("Number is Odd ");
+    }
+}
