bfe7589e29ce5eb400eed6b5f56817bca5cbbdde
Jatinsinghv64 <101871839+Jatinsinghv64@users.noreply.github.com>
2025-01-21 20:16:48 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter The First Number ");
+    scanf("%d",&a);
+    printf("Enter The Second Number ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("The answer is %d",c);
+}
